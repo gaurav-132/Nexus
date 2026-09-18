803bbca00d9b9966de75d95b3f07a9a0fd773df3
Gaurav Gusain <109432749+gaurav-132@users.noreply.github.com>
2026-09-18 19:59:46 +0530

    feat(db): register database module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { DatabaseModule } from './database/database.module.js';
+
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
+        DatabaseModule,
     ],
 })
 export class AppModule {}
